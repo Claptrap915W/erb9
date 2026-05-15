5845fc59ef4865e2d3cca50355406b72706e252a
claptrap915W <claptrap915@gmail.com>
2026-05-15 16:37:26 +0800

    step 5 : update listings template

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+from config import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls',namespace='pages')),
     path('listings/', include('listings.urls',namespace='listings')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,Document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+
+admin.site.site_header = "Clinic Administration"
+admin.site.site_title = "Clinic Admin Portal"
+admin.site.index_title = "Welcome to Clinic Admin Portal"
