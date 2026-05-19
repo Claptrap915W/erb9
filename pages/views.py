c0f6fd53e08692325acf4c29a3f08d00e0c79ad4
claptrap915W <claptrap915@gmail.com>
2026-05-19 12:48:47 +0800

    step 8 : search function done

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from listings.models import Listing
+from listings.choices import district_choices, room_type_choices, rooms_choices
 #from django.http import HttpResponse
 
 # def index(request):
@@ -6,7 +8,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "pages/index.html")
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {"listings" : listings, "district_choices": district_choices, 
+    "room_type_choices":room_type_choices,"rooms_choices":rooms_choices}
+    return render(request, "pages/index.html", context)
 
 def about(request):
     return render(request, "pages/about.html")
